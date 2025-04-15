3a88b7b29442804e5eb09d2289317cf17f0e8733
turgayyilmaz <iamtrgy@gmail.com>
2025-04-15 14:55:48 +0300

    lastones

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Country;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CountrySeeder extends Seeder
 {
@@ -14,41 +13,41 @@ class CountrySeeder extends Seeder
     public function run(): void
     {
         $countries = [
-            ['name' => 'United States', 'code' => 'US'],
-            ['name' => 'United Kingdom', 'code' => 'GB'],
-            ['name' => 'Canada', 'code' => 'CA'],
-            ['name' => 'Australia', 'code' => 'AU'],
-            ['name' => 'Germany', 'code' => 'DE'],
-            ['name' => 'France', 'code' => 'FR'],
-            ['name' => 'Italy', 'code' => 'IT'],
-            ['name' => 'Spain', 'code' => 'ES'],
-            ['name' => 'Netherlands', 'code' => 'NL'],
-            ['name' => 'Belgium', 'code' => 'BE'],
-            ['name' => 'Switzerland', 'code' => 'CH'],
-            ['name' => 'Austria', 'code' => 'AT'],
-            ['name' => 'Sweden', 'code' => 'SE'],
-            ['name' => 'Norway', 'code' => 'NO'],
-            ['name' => 'Denmark', 'code' => 'DK'],
-            ['name' => 'Finland', 'code' => 'FI'],
-            ['name' => 'Ireland', 'code' => 'IE'],
-            ['name' => 'Portugal', 'code' => 'PT'],
-            ['name' => 'Greece', 'code' => 'GR'],
-            ['name' => 'Poland', 'code' => 'PL'],
-            ['name' => 'Czech Republic', 'code' => 'CZ'],
-            ['name' => 'Hungary', 'code' => 'HU'],
-            ['name' => 'Estonia', 'code' => 'EE'],
-            ['name' => 'Turkey', 'code' => 'TR'],
-            ['name' => 'Japan', 'code' => 'JP'],
-            ['name' => 'China', 'code' => 'CN'],
-            ['name' => 'India', 'code' => 'IN'],
-            ['name' => 'Brazil', 'code' => 'BR'],
-            ['name' => 'Mexico', 'code' => 'MX'],
-            ['name' => 'South Africa', 'code' => 'ZA'],
-            ['name' => 'Russia', 'code' => 'RU'],
+            ['code' => 'EE', 'name' => 'Estonia', 'is_eu' => true],
+            ['code' => 'DE', 'name' => 'Germany', 'is_eu' => true],
+            ['code' => 'FR', 'name' => 'France', 'is_eu' => true],
+            ['code' => 'IT', 'name' => 'Italy', 'is_eu' => true],
+            ['code' => 'ES', 'name' => 'Spain', 'is_eu' => true],
+            ['code' => 'PT', 'name' => 'Portugal', 'is_eu' => true],
+            ['code' => 'GR', 'name' => 'Greece', 'is_eu' => true],
+            ['code' => 'AT', 'name' => 'Austria', 'is_eu' => true],
+            ['code' => 'IE', 'name' => 'Ireland', 'is_eu' => true],
+            ['code' => 'NL', 'name' => 'Netherlands', 'is_eu' => true],
+            ['code' => 'BE', 'name' => 'Belgium', 'is_eu' => true],
+            ['code' => 'LU', 'name' => 'Luxembourg', 'is_eu' => true],
+            ['code' => 'FI', 'name' => 'Finland', 'is_eu' => true],
+            ['code' => 'SE', 'name' => 'Sweden', 'is_eu' => true],
+            ['code' => 'DK', 'name' => 'Denmark', 'is_eu' => true],
+            ['code' => 'PL', 'name' => 'Poland', 'is_eu' => true],
+            ['code' => 'CZ', 'name' => 'Czech Republic', 'is_eu' => true],
+            ['code' => 'SK', 'name' => 'Slovakia', 'is_eu' => true],
+            ['code' => 'HU', 'name' => 'Hungary', 'is_eu' => true],
+            ['code' => 'RO', 'name' => 'Romania', 'is_eu' => true],
+            ['code' => 'BG', 'name' => 'Bulgaria', 'is_eu' => true],
+            ['code' => 'HR', 'name' => 'Croatia', 'is_eu' => true],
+            ['code' => 'SI', 'name' => 'Slovenia', 'is_eu' => true],
+            ['code' => 'LV', 'name' => 'Latvia', 'is_eu' => true],
+            ['code' => 'LT', 'name' => 'Lithuania', 'is_eu' => true],
+            ['code' => 'MT', 'name' => 'Malta', 'is_eu' => true],
+            ['code' => 'CY', 'name' => 'Cyprus', 'is_eu' => true],
+            // Non-EU countries
+            ['code' => 'GB', 'name' => 'United Kingdom', 'is_eu' => false],
+            ['code' => 'CH', 'name' => 'Switzerland', 'is_eu' => false],
+            ['code' => 'NO', 'name' => 'Norway', 'is_eu' => false],
         ];
 
         foreach ($countries as $country) {
-            Country::create($country);
+            DB::table('countries')->insertOrIgnore($country);
         }
     }
 }
